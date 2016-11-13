895948c37cdd5a04196c87e2a801e3f67234d75d
Yuwei Zhang <zhyuwei0115@gmail.com>
2016-11-13 15:32:53 -0500

    add new tables and integrate BMI

--- Data-analysis-sql/process.sql
+++ Data-analysis-sql/process.sql
@@ -165,3 +165,113 @@ from
 		where b.SUBJECT_ID is null
 		;
 
+create table bmi(
+"SUBJECT_ID" TEXT, 
+"BMI" REAL
+);
+
+.mode csv
+.import bmi_out.csv bmi
+
+select * from bmi limit 10;
+
+create table summary_bmi(
+"SUBJECT_ID" TEXT,
+"DOB" TEXT,
+"DOD" TEXT,
+"LIFESPAN" INTEGER,
+"HADM_ID" TEXT,
+"SEQ_NUM" TEXT,
+"ICD9_CODE" TEXT,
+"ADMITTIME" TEXT,
+"DISCHTIME" TEXT,
+"STAYTIME" INTEGER,
+"FREQ" INTEGER,
+"BMI" REAL
+);
+
+insert into summary_bmi
+select 
+summary.SUBJECT_ID, 
+DOB, 
+DOD, 
+LIFESPAN, 
+HADM_ID,
+SEQ_NUM, 
+ICD9_CODE,
+ADMITTIME, 
+DISCHTIME,
+STAYTIME,
+FREQ,
+BMI
+from summary left join bmi
+on summary.SUBJECT_ID = bmi.SUBJECT_ID;
+
+ALTER TABLE summary_bmi ADD COLUMN bmi_bin INTEGER;
+UPDATE summary_bmi 
+SET bmi_bin = 
+case when BMI is null then 0 
+when BMI < 25 then 1
+when BMI > 30 then 3 
+else 2 
+end;
+
+ALTER TABLE summary_bmi ADD COLUMN age_bin INTEGER;
+UPDATE summary_bmi 
+SET age_bin = 
+case when lifespan is null then 0 
+when lifespan < 65 then 1
+when lifespan > 75 then 3 
+else 2 
+end;
+
+ALTER TABLE summary_bmi ADD COLUMN lifespan_bin INTEGER;
+UPDATE summary_bmi 
+SET lifespan_bin = 
+case when lifespan is null then 0 
+when lifespan < 50 then 1
+when lifespan >= 50 and lifespan < 55 then 2
+when lifespan >= 55 and lifespan < 60 then 3
+when lifespan >= 60 and lifespan < 65 then 4
+when lifespan >= 65 and lifespan < 70 then 5
+when lifespan >= 70 and lifespan < 75 then 6
+when lifespan >= 75 and lifespan < 80 then 7
+when lifespan >= 80 and lifespan < 85 then 8
+when lifespan >= 85 and lifespan < 90 then 9
+else 10
+end;
+
+
+-- save bmi to csv file
+select 
+distinct SUBJECT_ID, 
+bmi, 
+bmi_bin 
+from summary_bmi 
+where bmi is not null
+limit 10;
+
+.mode csv
+.output bmi_bin.csv
+select 
+distinct SUBJECT_ID, 
+bmi, 
+bmi_bin,
+lifespan,
+lifespan_bin 
+from summary_bmi 
+where bmi is not null;
+.output stdout
+
+.mode csv
+.output bmi_graph.csv
+select 
+bmi_bin,
+lifespan_bin,
+count(distinct SUBJECT_ID)
+from summary_bmi 
+where bmi is not null
+group by bmi_bin, lifespan_bin
+order by bmi_bin ASC, lifespan_bin ASC
+;
+.output stdout
